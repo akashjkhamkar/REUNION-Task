552c1aebbe046dadde5c700f5e45c8ce76f471e3
akash <akash.khamkar40@gmail.com>
2022-01-23 22:08:36 +0530

    fixed the message on the /

--- endpoints/openEndpoints.js
+++ endpoints/openEndpoints.js
@@ -6,7 +6,7 @@ const { authenticateUser } = require('../queries');
 
 // These endpoints are open, hence any user can access them without authentication
 router.get('/', async (req, res) => {
-    res.json({'Message': "Hello, This is Akash', 'App': 'This API was created as an submission to the REUNON'S internship task"});
+    res.json({'Message': "Hello, This is Akash", 'App': "This API was created as an submission to the REUNON'S internship task"});
 })
 
 // validate the login data, if user exists in the db, give them a token
